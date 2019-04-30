a56f1e3ef35c76cc5a21df88284263055b47ffa2
Global Shark Movement Project <50103687+GlobalSharkMovement@users.noreply.github.com>
2019-04-30 15:17:56 +0100

    Delete Load_SharkData_monthly.R

--- GSMP_Rpackage/GSMP/data-raw/Load_SharkData_monthly.R
+++
@@ -1,7 +0,0 @@
-setwd("~/PhD/Shark data/WorldSharks/Global Fishery Interactions/Revision2/CleanScripts/GSMP/")
-devtools::use_data_raw()
-SharkData_monthly <-
-  read.csv('C:/Users/Marisa/Documents/PhD/Shark data/WorldSharks/Global Fishery Interactions/Revision2/CleanScripts/r_scripts/5_fig4/SharkData_monthly.csv')
-devtools::use_data(SharkData_monthly,  overwrite = TRUE)
-data('SharkData_monthly')
-head(SharkData_monthly)
