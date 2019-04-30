00c6e8d012d961509a85c7b4c5fe64542195c498
Global Shark Movement Project <50103687+GlobalSharkMovement@users.noreply.github.com>
2019-04-30 15:18:21 +0100

    Delete Load_trackPts.R

--- GSMP_Rpackage/GSMP/data-raw/Load_trackPts.R
+++
@@ -1,7 +0,0 @@
-setwd("~/PhD/Shark data/WorldSharks/Global Fishery Interactions/Revision2/CleanScripts/GSMP_Rpackage/GSMP/")
-devtools::use_data_raw()
-trackPts <-
-  read.csv('C:/Users/Marisa/Documents/PhD/Shark data/WorldSharks/Global Fishery Interactions/Revision2/CleanScripts/1a. Dit/TrackPoints.csv')
-devtools::use_data(trackPts)
-data('trackPts')
-head(trackPts)
